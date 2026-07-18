3665eff9a6da9825530ab0b224e6c4d9c4f1ecce
kinnguyn1678 <148688400+kinnguyn1678@users.noreply.github.com>
2026-07-17 21:26:37 -0600

    Hoan thanh Lab2 Bai 2

--- Lap2/Bai2_TinhDiem.c
+++ Lap2/Bai2_TinhDiem.c
@@ -11,8 +11,6 @@ int main() {
     scanf("%d", &ly);
     printf("Nhap diem Hoa: ");
     scanf("%d", &hoa);
-
-    // Ep kieu (float) de tranh phep chia lay phan nguyen
     diemTrungBinh = (float)(toan * 3 + ly * 2 + hoa) / 6;
 
     printf("Diem trung binh: %.2f\n", diemTrungBinh);
